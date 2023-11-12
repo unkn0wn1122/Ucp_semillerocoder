898bfd33ad85e57af5c35308d1e5c0f419c8a224
liandd <juan6.garcia@ucp.edu.co>
2023-11-12 03:15:04 -0500

    Listo funciona sistema de rutas test

--- pagina_semillero/src/App.jsx
+++ pagina_semillero/src/App.jsx
@@ -6,17 +6,6 @@ import Dashboard from './components/Dashboard';
 const App = () => {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-        </ul>
-      </nav>
-
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
